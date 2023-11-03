c235fa3d95bacfe47d70b76c9eadcd8d90baacf9
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-11-02 19:00:57 -0500

    typescript experimental integration

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,25 @@
+import { FontAwesome } from "@expo/vector-icons"
+import { Tabs } from "expo-router"
+
+export default function TabsLayout() {
+    return <Tabs
+        screenOptions={{
+            headerTitle: "",
+            tabBarLabelStyle: { textTransform: "capitalize" },
+            tabBarActiveTintColor: "black"
+        }}
+    >
+        <Tabs.Screen
+            name="route"
+            options={{
+                tabBarIcon: ({ focused, color }) => <FontAwesome name="code-fork" size={focused ? 32 : 24} color={color} />
+            }}
+        />
+        <Tabs.Screen
+            name="components"
+            options={{
+                tabBarIcon: ({ focused, color }) => <FontAwesome name="code" size={focused ? 32 : 24} color={color} />
+            }}
+        />
+    </Tabs>
+}
