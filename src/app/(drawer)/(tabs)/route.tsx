95c57922f205474490c517558e68b213f77b9ed2
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-11-07 22:22:52 -0500

    route: steps zig-zag position (sin func)

--- src/app/(drawer)/(tabs)/route.tsx
+++ src/app/(drawer)/(tabs)/route.tsx
@@ -1,28 +1,64 @@
 import Step from "@/components/Step"
-import { useEffect, useRef, useState } from "react"
-import { ScrollView, View } from "react-native"
+import { useEffect, useState } from "react"
+import { Dimensions, ScrollView, Text } from "react-native"
 
 export default function Route() {
-    const [coor, setCoor] = useState({ x: 0, y: 0 })
-    useEffect(() => {
-        console.log(coor)
-    }, [coor])
+
+    const [scroll, setScroll] = useState({ x: 0, y: 0 })
+    const WINDOW_WIDTH = Dimensions.get("window").width;
+
+    const stepsGenerator = (n: number, asc = true) => {
+        const mapX = {
+            [2 as number]: 0,
+            [1 as number]: 87,
+            [0 as number]: 173,
+            [-1 as number]: 259,
+            [-2 as number]: 346
+        }
+
+        let flag = asc;
+        let cont = 3
+        let acc = 0
+        const coorX = Array(n).fill(0).map((_, index) => {
+            if (index === 0) return mapX[acc]
+
+            if (flag) acc++
+            else acc--;
+
+            cont++
+            if (cont === 5) {
+                cont = 1
+                flag = !flag
+            }
+
+            return mapX[acc]
+        })
+
+        const coorY = Array(n).fill(0).map((_, index) => (index * Math.PI / 4) * 50);
+
+        return [coorX, coorY]
+    }
 
     return <ScrollView
         snapToEnd
-        ref={comp => comp?.scrollTo(coor)}
+        ref={comp => comp?.scrollTo(scroll)}
         style={{ flex: 1 }}
-        contentContainerStyle={{ justifyContent: 'center', alignItems: 'center' }}
+        contentContainerStyle={{ width: "90%", paddingHorizontal: "5%", justifyContent: 'center' }}
+        onContentSizeChange={(w) => {
+            console.log(WINDOW_WIDTH)
+            console.log(w)
+        }}
     >
-        <Step />
-        <Step />
-        <Step />
-        <Step />
-        <Step />
-        <Step />
-        <Step />
-        <Step />
-        <Step />
-        <Step setCoor={setCoor} />
+        {
+            stepsGenerator(10)[0].reverse().map((coorX, i) => (
+                <Step
+                    key={i}
+                    coorX={coorX}
+                    setCoor={i === 9 ? setScroll : undefined}
+                >
+                    <Text>{i}</Text>
+                </Step>
+            ))
+        }
     </ScrollView >
 }
--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -1,16 +1,19 @@
-import { useEffect, useRef } from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
-export default function Step(props: { children?: JSX.Element, setCoor?: ({ x, y }: { x: number, y: number }) => void }) {
+export default function Step(props: { children?: JSX.Element, coorX?: number, setCoor?: ({ x, y }: { x: number, y: number }) => void }) {
     return <View
-        onLayout={(e) => { props.setCoor && props.setCoor({ x: e.nativeEvent.layout.x, y: e.nativeEvent.layout.y }) }}
+        onLayout={(e) => {
+            props.setCoor && props.setCoor({ x: e.nativeEvent.layout.x, y: e.nativeEvent.layout.y })
+        }}
         style={{
             backgroundColor: "white", width: 80, height: 80,
             borderRadius: 10,
-            justifyContent: "center", alignItems: "center"
+            marginBottom: 10,
+            justifyContent: "center", alignItems: "center",
+            left: props.coorX || 0
         }}
     >
-        {/* props.children */}
-        <Text>Step</Text>
+        {props.children}
+        {/* <Text>Step</Text> */}
     </View>
 }
