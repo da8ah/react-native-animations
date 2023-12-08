0c3f3767acb0dc5a26c85f6a7d605bc8af933d1d
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-12-08 08:00:29 -0500

    route: viewer fade animation

--- src/components/Fade.tsx
+++ src/components/Fade.tsx
@@ -1,17 +1,25 @@
 import { useEffect } from "react";
 import { StyleSheet, View } from "react-native";
-import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withTiming } from "react-native-reanimated";
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withDelay, withRepeat, withSequence, withTiming } from "react-native-reanimated";
 
 export default function Fade() {
-    const duration = 500
-    const linear = useSharedValue(0)
+    const duration = 200
+    const shared = useSharedValue({ opacity: 0 })
 
     const animatedDefault = useAnimatedStyle(() => ({
-        transform: [{ translateY: linear.value }]
+        opacity: shared.value.opacity
     }))
 
     useEffect(() => {
-        linear.value = withRepeat(withTiming(linear.value - 50, { duration }), -1, true)
+        shared.value = withSequence(
+            withTiming({ opacity: shared.value.opacity }, { duration, easing: Easing.linear, }),
+            withTiming({ opacity: shared.value.opacity + 1 }, { duration: duration * 10, easing: Easing.linear, }),
+            withRepeat(withDelay(duration * 5,
+                withSequence(
+                    withDelay(duration * 1.25, withTiming({ opacity: shared.value.opacity }, { duration, easing: Easing.linear, })),
+                    withDelay(duration * 1.5, withTiming({ opacity: shared.value.opacity + 1 }, { duration: duration * 10, easing: Easing.linear, })),
+                )), -1, true)
+        )
     }, [])
 
     return <View style={styles.container}>
