50accd22640dbdb99b4d4cd7453b4d86b28a6eb6
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-12-12 10:37:27 -0500

    route: viewer typing

--- src/components/Typing.tsx
+++ src/components/Typing.tsx
@@ -1,31 +1,44 @@
 import { useEffect } from "react";
 import { TextInput, View } from "react-native";
-import Animated, { useAnimatedProps, useAnimatedStyle, useDerivedValue, useSharedValue, withDelay, withRepeat, withTiming } from "react-native-reanimated";
+import Animated, { useAnimatedProps, useAnimatedStyle, useSharedValue, withSequence, withTiming } from "react-native-reanimated";
 
 Animated.addWhitelistedNativeProps({ text: true })
 const AnimatedText = Animated.createAnimatedComponent(TextInput)
 
 export default function Typing() {
     const duration = 500
-    const shared = useSharedValue({ y: -10, bg: "blue" })
+    const shared1 = useSharedValue(-10)
+    const shared2 = useSharedValue(-10)
+    const shared3 = useSharedValue(-10)
 
-    const text = useDerivedValue(() => (
-        Math.round(Math.random() * 10).toString()
-    ))
-
-    const animatedProps = useAnimatedProps(() => ({
-        value: text.value, defaultValue: text.value
+    const animatedProps = useAnimatedProps(() => ({ value: "." }))
+    const animatedStyle1 = useAnimatedStyle(() => ({
+        transform: [{ translateY: shared1.value }]
     }))
-
-    const animatedStyle = useAnimatedStyle(() => ({
-        transform: [{ translateY: shared.value.y }],
-        backgroundColor: shared.value.bg
+    const animatedStyle2 = useAnimatedStyle(() => ({
+        transform: [{ translateY: shared2.value }]
+    }))
+    const animatedStyle3 = useAnimatedStyle(() => ({
+        transform: [{ translateY: shared3.value }]
     }))
 
     useEffect(() => {
-        shared.value = withRepeat(withDelay(duration * 2,
-            withTiming({ y: shared.value.y + 10, bg: "red" }, { duration }),
-        ), -1, true)
+        const interval = setInterval(() => {
+            shared1.value = withSequence(
+                withTiming(shared1.value + 10, { duration: duration * .1 }),
+                withTiming(shared1.value, { duration: duration * .1 }),
+            )
+            shared2.value = withSequence(
+                withTiming(shared2.value + 10, { duration: duration * .2 }),
+                withTiming(shared2.value, { duration: duration * .2 }),
+            )
+            shared3.value = withSequence(
+                withTiming(shared3.value + 10, { duration: duration * .3 }),
+                withTiming(shared3.value, { duration: duration * .3 }),
+            )
+        }, duration * 2)
+
+        return () => clearInterval(interval)
     }, [])
 
     return <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
@@ -33,19 +46,19 @@ export default function Typing() {
             animatedProps={animatedProps}
             editable={false}
             underlineColorAndroid="transparent"
-            style={[{ fontSize: 32 }, animatedStyle]}
+            style={[{ color: "black", width: 15, height: 15, justifyContent: "center", alignItems: "center", fontSize: 32, textAlign: "center" }, animatedStyle1]}
         />
         <AnimatedText
             animatedProps={animatedProps}
             editable={false}
             underlineColorAndroid="transparent"
-            style={[{ fontSize: 32 }, animatedStyle]}
+            style={[{ color: "black", width: 15, height: 15, justifyContent: "center", alignItems: "center", fontSize: 32, textAlign: "center" }, animatedStyle2]}
         />
         <AnimatedText
             animatedProps={animatedProps}
             editable={false}
             underlineColorAndroid="transparent"
-            style={[{ fontSize: 32 }, animatedStyle]}
+            style={[{ color: "black", width: 15, height: 15, justifyContent: "center", alignItems: "center", fontSize: 32, textAlign: "center" }, animatedStyle3]}
         />
     </View>
 }
