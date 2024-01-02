2c1292eb202f0bad973a46929b31a2b9609657e6
Tiber <danilo.ochoa.hidalgo@outlook.com>
2024-01-02 10:17:38 -0500

    guides: viewer intro part1 flow

--- src/components/guides/Intro.tsx
+++ src/components/guides/Intro.tsx
@@ -4,21 +4,34 @@ import Animated, { Easing, useAnimatedStyle, useSharedValue, withSequence, withT
 
 export default function Intro() {
     const duration = 200
-    const shared = useSharedValue<{ opacity?: number, scale: number, rotate: number }>({ opacity: 0, scale: 0, rotate: 0 })
+    const shared = useSharedValue({
+        opacity: 0,
+        width: 0,
+        height: 0,
+        translateX: 0,
+        translateY: 0,
+        rotate: 0,
+    })
 
     const animatedDefault = useAnimatedStyle(() => ({
         opacity: shared.value.opacity,
+        width: shared.value.width,
+        height: shared.value.height,
         transform: [
-            { scale: shared.value.scale },
+            { translateX: shared.value.translateX },
+            { translateY: shared.value.translateY },
             { rotateZ: `${shared.value.rotate}deg` },
         ]
     }))
 
     useEffect(() => {
         shared.value = withSequence(
-            withTiming({ opacity: 0, scale: .1, rotate: 0 }, { duration, easing: Easing.linear, }),
-            withTiming({ opacity: .5, scale: .5, rotate: 360 }, { duration, easing: Easing.linear, }),
-            withTiming({ opacity: 1, scale: 1, rotate: 720 }, { duration, easing: Easing.linear, }),
+            withTiming({ opacity: 0, width: 10, height: 10, translateX: 10, translateY: 10, rotate: 0 }, { duration }),
+            withTiming({ opacity: 1, width: 20, height: 20, translateX: -20, translateY: -20, rotate: 0 }, { duration }),
+            withTiming({ opacity: 1, width: 20, height: 20, translateX: -10, translateY: -30, rotate: 0 }, { duration }),
+            // withTiming({ opacity: 0, scale: .1, rotate: 0 }, { duration, easing: Easing.linear, }),
+            // withTiming({ opacity: .5, scale: .5, rotate: 360 }, { duration, easing: Easing.linear, }),
+            // withTiming({ opacity: 1, scale: 1, rotate: 720 }, { duration, easing: Easing.linear, }),
         )
     }, [])
 
@@ -37,7 +50,7 @@ const styles = StyleSheet.create({
     },
     animatedView: {
         backgroundColor: "red",
-        borderRadius: 10,
+        borderRadius: 100,
         justifyContent: "center",
         alignItems: "center"
     }
