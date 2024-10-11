a9b425ae059940be31f55408aecf676ab603b35a
Tiber <danilo.ochoa.hidalgo@outlook.com>
2024-10-11 17:53:26 -0500

    guides: viewer intro animation 2 times jump

--- src/components/guides/Intro.tsx
+++ src/components/guides/Intro.tsx
@@ -4,78 +4,103 @@ import Animated, { Easing, useAnimatedStyle, useSharedValue, withSequence, withT
 
 export default function Intro() {
 
-    const { height, width } = useWindowDimensions()
-    const duration = 200
-    const dimentions = useSharedValue({
-        opacity: 0,
-        width: 0,
-        height: 0,
-    })
-    const location = useSharedValue({
-        translateX: width / 4,
-        translateY: height / 2,
-        rotate: 0,
-    })
+  const { height, width } = useWindowDimensions()
+  const duration = 200
+  const dimentions = useSharedValue({
+    opacity: 0,
+    width: 0,
+    height: 0,
+  })
+  const location = useSharedValue({
+    top: height / 2,
+    left: width / 4,
+  })
 
-    const animatedDefault = useAnimatedStyle(() => ({
-        opacity: dimentions.value.opacity,
-        width: dimentions.value.width,
-        height: dimentions.value.height,
-        transform: [
-            { translateX: location.value.translateX },
-            { translateY: location.value.translateY },
-            { rotateZ: `${location.value.rotate}deg` },
-        ]
-    }))
+  const animatedDefault = useAnimatedStyle(() => ({
+    opacity: dimentions.value.opacity,
+    width: dimentions.value.width,
+    height: dimentions.value.height,
+    top: location.value.top,
+    left: location.value.left,
+  }))
 
 
 
-    const launchAnimation = () => {
-        console.log(height, width / 4)
+  const launchAnimation = () => {
+    dimentions.value =
+      withSequence(
+        withTiming(
+          { opacity: 0, width: 0, height: 0, },
+          { duration, easing: Easing.inOut(Easing.quad) }
+        ),
+        withTiming(
+          { opacity: 1, width: 20, height: 20 },
+          { duration, easing: Easing.inOut(Easing.quad) }
+        ),
+        withSequence(
+          withTiming(
+            { opacity: 0, width: 0, height: 0, },
+            { duration, easing: Easing.inOut(Easing.quad) }
+          ),
+          withTiming(
+            { opacity: 1, width: 20, height: 20 },
+            { duration, easing: Easing.inOut(Easing.quad) }
+          ),
+        ),
+      )
 
-        dimentions.value = withSequence(
-            withTiming({ opacity: 0, width: 0, height: 0, }, { duration }),
-            withTiming({ opacity: 1, width: 10, height: 10, }, { duration }),
-             withTiming({ opacity: 1, width: 20, height: 20, }, { duration })
-        )
-        location.value = withSequence(
-            withTiming({ translateX: width / 4, translateY: height / 2, rotate: 0 }, { duration }),
-            withTiming({ translateX: (width / 4) + 10, translateY: (height / 2) + 10, rotate: 0 }, { duration })
-        )
+    const step = 10
+    location.value =
+      withSequence(
+        withTiming(
+          { left: width / 4, top: height / 3 + step },
+          { duration, easing: Easing.inOut(Easing.quad) }
+        ),
+        withTiming(
+          { left: width / 4 + step, top: height / 3 - (step ** 2) },
+          { duration, easing: Easing.inOut(Easing.quad) }
+        ),
+        withTiming(
+          { left: width / 4 + step, top: height / 3 },
+          { duration, easing: Easing.inOut(Easing.quad) }
+        ),
+        withSequence(
+          withTiming(
+            { left: width / 4, top: height / 3 + (step * 1.2) },
+            { duration, easing: Easing.inOut(Easing.quad) }
+          ),
+          withTiming(
+            { left: width / 4 + (step * 5), top: height / 3 - ((step * 1.2) ** 2) },
+            { duration, easing: Easing.inOut(Easing.quad) }
+          ),
+          withTiming(
+            { left: width / 4 + (step * 5), top: height / 3 },
+            { duration, easing: Easing.inOut(Easing.quad) }
+          ),
+        ),
+      )
+  }
 
-        Array(10).fill(null).forEach((_, index) => {
-            console.log(index)
-
-
-            location.value =
-                // withSequence(
-                withTiming({ translateX: index, translateY: index, rotate: 0 }, { duration })
-            // withTiming({ opacity: 1, width: 20, height: 20, translateX: -10, translateY: -30, rotate: 0 }, { duration }),
-            // withTiming({ opacity: 0, scale: .1, rotate: 0 }, { duration, easing: Easing.linear, }),
-            // withTiming({ opacity: .5, scale: .5, rotate: 360 }, { duration, easing: Easing.linear, }),
-            // withTiming({ opacity: 1, scale: 1, rotate: 720 }, { duration, easing: Easing.linear, }),
-            // )
-        })
-    }
-
-    return <TouchableOpacity style={styles.container} onPress={launchAnimation}>
-        <Animated.View style={[styles.animatedView, animatedDefault]}>
-            <Text>Reanimated 3</Text>
-        </Animated.View>
-    </TouchableOpacity>
+  return <TouchableOpacity style={styles.container} onPress={launchAnimation}>
+    <Animated.View style={[styles.animatedView, animatedDefault]}>
+      <Text>Reanimated 3</Text>
+    </Animated.View>
+  </TouchableOpacity>
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: '100%',
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    animatedView: {
-        backgroundColor: "red",
-        borderRadius: 100,
-        justifyContent: "center",
-        alignItems: "center"
-    }
+  container: {
+    position: "relative",
+    flex: 1,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  animatedView: {
+    position: "absolute",
+    backgroundColor: "red",
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 })
